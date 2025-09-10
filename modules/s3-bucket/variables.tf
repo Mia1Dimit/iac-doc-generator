0c57de92b711adc07671f6965562320d2d8f5994
Mia1Dimit <jimiaoul@gmail.com>
2025-09-10 11:44:02 +0200

    small change

--- modules/s3-bucket/variables.tf
+++ modules/s3-bucket/variables.tf
@@ -26,7 +26,7 @@ variable "enable_versioning" {
 }
 
 variable "tags" {
-  description = "Additional tags to apply to some resources"
+  description = "Additional tags to apply to some of our resources"
   type        = map(string)
   default     = {}
 }
